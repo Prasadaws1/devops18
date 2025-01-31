541da303cd82db85b4b204bcee7853d75225e1e2
prasad <prasadyadav5906@gmail.com>
2025-01-31 16:17:17 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,25 +2,7 @@ resource "aws_s3_bucket" "one" {
   bucket = "gitops-k8s-bucket.yml"
 }
 
-resource "aws_s3_bucket_ownership_controls" "two" {
-  bucket = aws_s3_bucket.one.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
 
-resource "aws_s3_bucket_acl" "three" {
-  depends_on = [aws_s3_bucket_ownership_controls.two]
 
-  bucket = aws_s3_bucket.one.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_versioning" "three" {
-bucket = aws_s3_bucket.one.id
-versioning_configuration {
-status = "Enabled"
-}
-}
 
 
